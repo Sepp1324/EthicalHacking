4daebf27123570faffd7cefbf17b02da2672fd29
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-17 18:31:56 +0200

    Worked on Mac-Changer

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -9,7 +9,14 @@ def get_arguments():
 
 	parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's Mac-Adress")
 	parser.add_option("-m", "--mac", dest="new_mac", help="New Mac-Adress")
-	return parser.parse_args()
+	(options, arguments) = parser.parse_args()
+
+	if not options.interface:
+		parser.error("[-] You need to specify an interface! --help")
+
+	elif not options.new_mac:
+		parser.error("[-] You need to specify a Mac-Adress! --help")
+	return options
 
 
 def change_mac(interface, new_mac):
@@ -20,7 +27,8 @@ def change_mac(interface, new_mac):
 	subprocess.call(["ifconfig", interface, "up"])	
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)	
+
 
 print("[+] Changed Mac-Adress successfully to " + options.new_mac + "!")
