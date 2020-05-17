aa3b35630a8d3884f06cc73f7ada4cafa6ebdb3a
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-17 15:38:02 +0200

    Fixed Mac-Changer

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -12,7 +12,7 @@ print("[+] Changing Mac-Adress for interface " + interface)
 #subprocess.call("ifconfig " + interface +" up", shell=True)
 
 subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw ether", new_mac])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 subprocess.call(["ifconfig", interface, "up"])
 
 print("[+] Changed Mac-Adress successfully to " + new_mac + "!")
