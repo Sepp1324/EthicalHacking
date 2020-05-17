4a1e383b3669c72c64c309f23851cf18e8759e05
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-17 18:09:28 +0200

    Cleanup Code

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -4,6 +4,14 @@ import subprocess
 import optparse
 
 
+def get_arguments():
+	parser = optparse.OptionParser()
+
+	parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's Mac-Adress")
+	parser.add_option("-m", "--mac", dest="new_mac", help="New Mac-Adress")
+	return parser.parse_args()
+
+
 def change_mac(interface, new_mac):
 	print("[+] Changing Mac-Adress for interface " + interface)
 
@@ -12,13 +20,7 @@ def change_mac(interface, new_mac):
 	subprocess.call(["ifconfig", interface, "up"])	
 
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's Mac-Adress")
-parser.add_option("-m", "--mac", dest="new_mac", help="New Mac-Adress")
-
-(options, arguments) = parser.parse_args()
-
-change_mac(options.interface, options.new_mac)
+(options, arguments) = get_arguments()
+change_mac(options.interface, options.new_mac)	
 
 print("[+] Changed Mac-Adress successfully to " + options.new_mac + "!")
