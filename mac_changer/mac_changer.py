edbdbdc604531eef9cc111838e0c6c02279fc4e8
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-17 15:33:37 +0200

    Improved mac_changer-py

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -7,8 +7,12 @@ new_mac = raw_input("New Mac-Adress: ")
 
 print("[+] Changing Mac-Adress for interface " + interface)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface +" hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface +" up", shell=True)
+#subprocess.call("ifconfig " + interface + " down", shell=True)
+#subprocess.call("ifconfig " + interface +" hw ether " + new_mac, shell=True)
+#subprocess.call("ifconfig " + interface +" up", shell=True)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
 
 print("[+] Changed Mac-Adress successfully to " + new_mac + "!")
