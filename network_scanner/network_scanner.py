fd082f035042371a2d89bf5f17d396cf8d60a834
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-18 22:31:27 +0200

    Worked on ARP-Spoofer

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -5,7 +5,10 @@ import scapy.all as scapy
 
 def scan(ip):
 	arp_req = scapy.ARP(pdst=ip)
-	print(arp_req.summary())
+	bc = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+	arp_req_bc = bc/arp_req
+	print(arp_req_bc.summary())
+	arp_req_bc.show()
 
 
 scan("10.0.0.138/24")
