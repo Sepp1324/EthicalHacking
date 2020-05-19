3412171b1456cb01536c9a2274c38cf5318ae410
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-19 17:38:48 +0200

    Worked on Network-Scanner

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -5,7 +5,10 @@ import scapy.all as scapy
 
 def scan(ip):
 	arp_req = scapy.ARP(pdst=ip)
-	print(arp_req.summary())
+	bc = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+	arp_req_bc = bc/arp_req
+	anwsered, unanwsered = scapy.srp(arp_req_bc, timeout=1)
+	print(unanwsered.summary())
 
 
-scan("10.0.0.1/24")
+scan("10.0.0.138/24") 
