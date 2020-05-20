2a298d0946030e7af00cfc58ac1a32fe4dc5b9ab
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-20 10:46:31 +0200

    Worked on Network-Scanner

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -7,12 +7,13 @@ def scan(ip):
 	arp_req = scapy.ARP(pdst=ip)
 	bc = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 	arp_req_bc = bc/arp_req
-	anwsered = scapy.srp(arp_req_bc, timeout=1)[0]
+	anwsered = scapy.srp(arp_req_bc, timeout=1, verbose=False)[0]
+
+	print("IP\t\t\tMac-Adress")
+	print("-" * 41)
 
 	for anwser in anwsered:
-		print(anwser[1].psrc)
-		print(anwser[1].hwsrc)
-		print("----------------------------------------------------------------")
+		print(anwser[1].psrc + "\t\t" + anwser[1].hwsrc)
 
 
 scan("10.0.0.138/24") 
