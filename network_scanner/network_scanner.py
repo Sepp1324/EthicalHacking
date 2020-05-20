6a0223eede7b22aa6c26eca6b9f0e907eba24db4
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-20 21:41:23 +0200

    Worked on network-scanner

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -12,8 +12,13 @@ def scan(ip):
 	print("IP\t\t\tMac-Adress")
 	print("-" * 41)
 
+	clients = []
+
 	for anwser in anwsered:
+		clients_dict = {"ip": anwser[1].psrc, "mac": anwser[1].hwsrc}
+		clients.append(clients_dict)
 		print(anwser[1].psrc + "\t\t" + anwser[1].hwsrc)
+	print(clients)
 
 
 scan("10.0.0.138/24") 
