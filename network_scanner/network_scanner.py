05c06618dbd97150a9703101af4b0ce40cfa1992
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-20 21:50:51 +0200

    Implemented Dictionaries

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -9,16 +9,19 @@ def scan(ip):
 	arp_req_bc = bc/arp_req
 	anwsered = scapy.srp(arp_req_bc, timeout=1, verbose=False)[0]
 
-	print("IP\t\t\tMac-Adress")
-	print("-" * 41)
-
 	clients = []
 
 	for anwser in anwsered:
 		clients_dict = {"ip": anwser[1].psrc, "mac": anwser[1].hwsrc}
 		clients.append(clients_dict)
-		print(anwser[1].psrc + "\t\t" + anwser[1].hwsrc)
-	print(clients)
+	return clients
 
 
-scan("10.0.0.138/24") 
+def print_result(results):
+	print("IP\t\t\tMac-Adress\n" + "-" * 41)
+
+	for client in results:
+		print(client["ip"] + "\t\t" + client["mac"])
+
+scan_res = scan("10.0.0.138/24") 
+print_result(scan_res)
